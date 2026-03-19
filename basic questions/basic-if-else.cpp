49a1f1206dfcab1af9788409eccde66e965e4498
Sankalp Lakhera <136921400+Sankalp00007@users.noreply.github.com>
2026-03-19 22:57:29 +0530

    Add files via upload

---
+++ basic questions/basic-if-else.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int a , b;
+    // cin>> a;
+    // cin>> b;
+
+    // cout<< a+b;
+
+    //typecasting
+
+    // int a = 65;
+    // char c ='b';
+    // c=a;
+    // cout<< c;
+    
+    // odd even check 
+
+    // int num;
+    // cout<<"enter the number"<<endl;
+    // cin >> num;
+
+    // if(num%2==0){
+    //     cout<<"even"<<endl;
+    // }
+
+    // else{
+    //     cout<<"odd"<<endl;
+    // }
+
+    // if , else if , else
+    // int num;
+    // cout<<"enter the number to check +ve , -ve , 0"<<endl;
+    // cin>> num;
+
+    // if(num>0){
+    //     cout<< "positive"<<endl;
+    // }
+
+    // else if(num==0){
+    //     cout<< 0;
+    // }
+
+    // else{
+    //     cout<<"the given number is negative";
+    // }
+
+    // char c;
+    // cin >> c;
+
+    // if(c == 'a'){
+    //     cout<<"vowel"<<endl;
+    // }
+
+    // else if(c == 'e'){
+    //      cout<<"vowel"<<endl;
+    // }
+
+    // else if(c == 'i'){
+    //      cout<<"vowel"<<endl;
+    // }
+
+    // else if(c == 'o'){
+    //      cout<<"vowel"<<endl;
+    // }
+
+    // else if(c == 'u'){
+    //      cout<<"vowel"<<endl;
+    // }
+
+    // else{
+    //     cout<<"consonent"<<endl;
+    // }
+
+    // loops
+
+    // for (int i=1; i<=10;i++){
+    //     cout<<"hello coder army "<<endl;
+    // }
+
+    // int n;
+
+    // cout<< "enter value of n to print squares upto n"<<endl;
+    // cin>>n;
+
+    // for ( int i=0; i<=n ; i++){
+    //     cout<< i<<" square is : " << i*i <<endl;
+    // }
+
+    int n ;
+
+    cin>> n;
+
+    for(int i=1; i<=n ; i++){
+        if(i%2==0){
+             cout<< i<< endl;
+        }
+        else{
+
+        }
+           
+    }
+
+
+
+}
